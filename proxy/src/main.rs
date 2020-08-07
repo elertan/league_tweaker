96ab46c43196e0fb86d3fb0483485a0ab3a40f2a
Dennis Kievits <denkievits@gmail.com>
2020-08-07 18:41:21 +0200

    Porgress

--- proxy/src/main.rs
+++ proxy/src/main.rs
@@ -1,6 +1,11 @@
+use futures::future::Future;
+use futures::prelude::*;
 use std::env;
+use tokio::runtime::Builder;
+use websocket::ClientBuilder;
 
-fn main() {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = env::args().collect::<Vec<String>>();
 
     let remoting_auth_token_arg = args
@@ -15,7 +20,8 @@ fn main() {
     let remoting_auth_token = remoting_auth_token_arg
         .split("=")
         .nth(1)
-        .expect("Remoting auth token has unexpected format");
+        .expect("Remoting auth token has unexpected format")
+        .to_string();
     let app_port = app_port_arg
         .split("=")
         .nth(1)
@@ -28,4 +34,19 @@ fn main() {
         format!("token: {} port: {}", remoting_auth_token, app_port),
     )
     .expect("Could not write to file");
+
+    let ws_conn_string = format!("ws://127.0.0.1:{}", app_port);
+
+    let auth_header_data = format!("riot:{}", &remoting_auth_token);
+    let auth_header = format!("Basic {}", base64::encode(auth_header_data));
+
+    let mut headers = websocket::header::Headers::new();
+    headers.set(websocket::header::Authorization(auth_header));
+    let client = ClientBuilder::new(ws_conn_string.as_str())?
+        .custom_headers(&headers)
+        .connect_insecure()
+        .unwrap();
+    let (sink, stream) = client.split().unwrap();
+
+    Ok(())
 }
