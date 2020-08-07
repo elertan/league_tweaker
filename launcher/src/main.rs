fc18c320677a13cdd5c7030ecf04d0235786f087
Dennis Kievits <denkievits@gmail.com>
2020-08-07 18:52:42 +0200

    Copy over

--- launcher/src/main.rs
+++ launcher/src/main.rs
@@ -2,6 +2,12 @@ fn main() {
     println!("Launching launcher...");
     let result = std::process::Command::new("C:\\Riot Games\\League of Legends\\LeagueClient.exe")
         .status()
-        .unwrap();
+        .expect("Failed to LeagueClient");
     println!("Status: {}", result);
+
+    std::fs::copy(
+        "..\\..\\..\\proxy\\target\\release\\proxy.exe",
+        "C:\\Riot Games\\League of Legends\\LeagueClientUx.exe",
+    )
+    .expect("Failed to copy over proxy");
 }
