0284dce98c1f5f2a891585690a96bfe54ed9aeb0
Dennis Kievits <denkievits@gmail.com>
2020-08-13 22:04:54 +0200

    Fix

--- forwarder/src/main.rs
+++ forwarder/src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    let args = env::args().collect::<Vec<String>>();
+    let args = std::env::args().collect::<Vec<String>>();
     std::process::Command::new("C:\\Riot Games\\League of Legends\\proxy.exe")
         .args(args.as_slice())
         .spawn()
